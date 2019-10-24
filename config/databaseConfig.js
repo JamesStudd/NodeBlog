b5344409ad03daa168216dbfb908bd355e105537
James Studd <james.studd@live.co.uk>
2019-10-24 21:20:34 +0100

    Removed unused files

--- config/databaseConfig.js
+++
@@ -1,4 +0,0 @@
-let env = process.env.NODE_ENV || 'development';
-const config = require('./../credentials')[env];
-
-process.env.MONGODB_URI = `mongodb://${config.user}:${config.password}${config.database}`;
