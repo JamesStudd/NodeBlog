e823ebc1a1ee071510bcf17857d88e59d69c9ddf
jamesstudd <james.studd@live.co.uk>
2020-08-24 22:37:22 +0100

    Changed project schema

--- database/models/projectModel.js
+++ database/models/projectModel.js
@@ -1,46 +1,50 @@
-const mongoose = require('./../database');
+const mongoose = require("./../database");
 
 let Project = undefined;
-if (mongoose.connection && mongoose.connection.readyState == 1 || mongoose.connection.readyState == 2) {
-    let projectSchema = mongoose.Schema({
-        title: {
-            type: String,
-            required: true
-        },
-        link: {
-            type: String
-        },
-        categories: {
-            type: [String],
-            default: []
-        },
-        keywords: {
-            type: [String],
-            default: []
-        },
-        shortDescription: {
-            type: String,
-            required: false
-        },
-        longDescription: {
-            type: String,
-            required: true
-        },
-        parsedHtml: {
-            type: String,
-            required: false
-        },
-        date: {
-            type: Date,
-            default: Date.now
-        },
-        image: {
-            type: String,
-            required: false
-        }
-    });
+if (
+	(mongoose.connection && mongoose.connection.readyState == 1) ||
+	mongoose.connection.readyState == 2
+) {
+	let projectSchema = mongoose.Schema({
+		title: {
+			type: String,
+			required: true,
+		},
+		link: {
+			type: String,
+		},
+		categories: {
+			type: [String],
+			default: [],
+		},
+		priority: {
+			type: [String],
+			enum: ["low", "medium", "high"],
+			default: "low",
+		},
+		shortDescription: {
+			type: String,
+			required: false,
+		},
+		longDescription: {
+			type: String,
+			required: true,
+		},
+		parsedHtml: {
+			type: String,
+			required: false,
+		},
+		date: {
+			type: Date,
+			default: Date.now,
+		},
+		image: {
+			type: String,
+			required: false,
+		},
+	});
 
-    Project = mongoose.model('Project', projectSchema);
-};
+	Project = mongoose.model("Project", projectSchema);
+}
 
 module.exports = Project;
